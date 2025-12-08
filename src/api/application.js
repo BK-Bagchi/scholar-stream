e0d2df832c7d2d5a2606a06ebbc6caa69e89745b
unknown <bkbagchi.dipto@gmail.com>
2025-12-08 16:14:42 +0600

    api added to update application and status

--- src/api/application.js
+++ src/api/application.js
@@ -3,3 +3,7 @@ import axios from "./axiosInstance";
 export const applyScholarship = (data) => axios.post("/application/post", data);
 export const getAllApplications = () => axios.get("/application/all");
 export const getUserApplications = () => axios.get("/application/user");
+export const updateApplication = (id, data) =>
+  axios.put(`/application/update/${id}`, data);
+export const updateApplicationStatus = (id, data) =>
+  axios.patch(`/application/update-status/${id}`, data);
